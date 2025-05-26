658efd6c6a2c5401e2d6a4b3352a3712af04109d
Birgül Özhan <birgulozhan9@gmail.com>
2025-05-26 23:47:21 +0300

    proje oluşturuldu

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { nanoid } from 'nanoid';
+import { addTasks } from '../redux/features/tasks/taskSlice'; 
+
+
+const TaskForm = () => {
+    const [text, setText] = useState('');
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(text.trim() === '') return;
+
+        dispatch(addTasks({
+            id: nanoid(),
+            text: text.trim(),
+            completed: false,
+        }))
+
+        setText('');
+    }
+
+  return (
+    <form onSubmit={handleSubmit} className='flex items-center gap-2 mb-4'>
+        <input 
+        type="text" 
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder='Add new task' 
+        className='flex-1 p-2 border rounded-md' 
+        />
+
+        <button type='submit' className='bg-blue-500 text-white px-6 py-2 rounded hover:text-blue-600'>Add Task</button>
+    </form>
+  )
+}
+
+export default TaskForm
